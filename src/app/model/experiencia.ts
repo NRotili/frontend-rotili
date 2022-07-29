1716e5b86acb2f71f35790ef9ca54c31453fecfe
NRotili <rotilinicolas@gmail.com>
2022-07-28 22:17:36 -0300

    act. 2022-07-28

---
+++ src/app/model/experiencia.ts
@@ -0,0 +1,14 @@
+export class Experiencia {
+    id?: number;
+    nombreE: string;
+    descripcionE: string;
+    inicioE: Date;
+    finE: Date;
+
+    constructor(nombreE: string, descripcionE: string, inicioE: Date, finE: Date){
+        this.nombreE = nombreE;
+        this.descripcionE = descripcionE;
+        this.inicioE = inicioE;
+        this.finE = finE;
+    }
+}
